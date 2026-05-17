a48eb56e9eed4d92a4a7c56597d43b2f1ccf8392
andreiM <andreimarculesteanu28@gmail.com>
2026-05-17 03:17:14 +0200

    added fixd typedef to FLOOD.h

--- FLOOD.h
+++ FLOOD.h
@@ -72,6 +72,8 @@ static int displacements[CONTIGUOUS_CELLS][2] = {
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 #define SQR(a) ((a) * (a))
 
+typedef ap_fixed<16, 8> fixd;
+
 /*
  * Utils: Macro function to simplify accessing data of 2D and 3D matrixes stored in a flattened array
  * 	These macro-functions can be changed and/or optimized by the students
